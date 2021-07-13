8f63dcb616bf3ade6139c07e5b9f68006f89f48a
yasin459 <yasinnmoosavi@gmail.com>
2021-07-13 23:48:44 +0430

    useless controller deleted future amalkard added

--- lib/views/subject_page/subject_page.dart
+++ lib/views/subject_page/subject_page.dart
@@ -1,32 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:http/http.dart' as http;
-import 'package:mobdev_game_project/controllers/subject_page_controller.dart';
 import 'package:mobdev_game_project/views/subject_page/subject.dart';
 import 'package:mobdev_game_project/views/subject_page/laoding.dart';
+import 'package:parse_server_sdk/parse_server_sdk.dart';
 
 class SubjectPage extends StatelessWidget {
+  Future<List<dynamic>?> getSubjects() async {
+    QueryBuilder<ParseObject> queryTodo =
+        QueryBuilder<ParseObject>(ParseObject('Subject'));
+    final ParseResponse apiResponse = await queryTodo.query();
+
+    if (apiResponse.success && apiResponse.results != null) {
+      return apiResponse.results!;
+    } else {
+      print("we fucked up");
+      return [];
+    }
+  }
+
   //todo fix fetching async data
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         body: Container(
-            child: GetX<SubjectPageController>(
-                init: SubjectPageController(),
-                builder: (controller) {
-                  if ((controller.subjects() as List<String>).length == 0) {
+            child: FutureBuilder(
+                future: getSubjects(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData ||
+                      (snapshot.data as List<dynamic>).length == 0) {
                     return LoadingSupportPage();
                   } else {
                     return Column(children: [
                       Expanded(
-                        child: Padding(
-                          padding: EdgeInsets.only(
-                            top: 4,
-                            bottom: 4,
-                          ),
-                          child: Center(
-                            child: Text("لطفا یکیرو انتخاب کن"),
+                        child: Center(
+                          child: Padding(
+                            padding: EdgeInsets.all(4),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Center(
+                                  child: Text("لطفا یکیرو انتخاب کن"),
+                                ),
+                                Wrap(children: [
+                                  Icon(Icons.favorite),
+                                  Text("24")
+                                ]),
+                              ],
+                            ),
                           ),
                         ),
                         flex: 1,
@@ -36,9 +57,9 @@ class SubjectPage extends StatelessWidget {
                         child: GridView.count(
                           crossAxisCount: 2,
                           childAspectRatio: 3,
-                          children:
-                              controller.subjects().map<Widget>((subject) {
-                            return SubjectWidget(subject);
+                          children: (snapshot.data as List<dynamic>)
+                              .map<Widget>((subject) {
+                            return SubjectWidget(subject.get<String>('name'));
                           }).toList(),
                           // children: ccc.map((e) => Text(e)).toList(),
                         ),
