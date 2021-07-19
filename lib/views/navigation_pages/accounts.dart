7ddcb999264f481f759227cc17a4b5ef03c874e8
Peyman-hme <peyman.eb13@gmail.com>
2021-07-20 03:00:21 +0430

    Login and Register menu now is more complete (AuthButton and error handler added)

--- lib/views/navigation_pages/accounts.dart
+++ lib/views/navigation_pages/accounts.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mobdev_game_project/models/user.dart';
+import 'package:mobdev_game_project/views/navigation_pages/home.dart';
+import 'package:auth_buttons/auth_buttons.dart';
 
 class AccountsPageProfile extends StatelessWidget {
   const AccountsPageProfile({Key? key}) : super(key: key);
@@ -43,9 +45,53 @@ class AccountsPageLogin extends StatelessWidget {
                 onPressed: () {
                   User.forParse(controller.usernameController.text,
                           controller.passwordController.text)
-                      .login();
+                      .login()
+                      .then((response) {
+                    if (response.success) {
+                      Get.off(HomePage());
+                      Get.snackbar("عملیات موفق", "با موفقیت وارد اکانتت شدی",backgroundColor: Colors.black,colorText: Colors.white);
+                    } else {
+                      String errorMessage = "";
+                      switch (response.error!.code) {
+                        case 100:
+                          {errorMessage = "ارتباط با سرور برقرار نشد!";}
+                          break;
+                        case 101:
+                          {errorMessage = "نام کاربری یا رمزعبور اشتباه است";}
+                          break;
+                        case 200:
+                          {errorMessage = "یادت رفت نام کاربریت رو وارد کنی!";}
+                          break;
+                        case 201:
+                          {errorMessage = "یادت رفت رمزعبورت رو وارد کنی!";}
+                          break;
+                        default:
+                          {response.toString();}
+                          break;
+                      }
+                      Get.defaultDialog(
+                        title: "خطا",
+                        titleStyle: TextStyle(color: Colors.red),
+                        content: Text(
+                          errorMessage,
+                        ),
+                        textConfirm: "باشه"
+                      );
+                    }
+                    return response;
+                  });
                 },
                 child: Text("ورود")),
+            GoogleAuthButton(
+              onPressed: () {
+                // your implementation
+              },
+              isLoading: false,
+              style: const AuthButtonStyle(
+                buttonType: AuthButtonType.secondary,
+                iconType: AuthIconType.outlined,
+              ),
+            ),
             TextButton(
                 onPressed: () {
                   Get.to(AccountsPageRegister());
@@ -95,7 +141,46 @@ class AccountsPageRegister extends StatelessWidget {
                   User.forParse(controller.usernameController.text,
                           controller.passwordController.text,
                           emailAddress: controller.emailController.text)
-                      .signUp();
+                      .signUp().then((response) {
+                    if (response.success) {
+                      Get.off(HomePage());
+                      Get.snackbar("عملیات موفق", "ثبت نامت با موفقیت انجام شد",backgroundColor: Colors.black,colorText: Colors.white);
+                    } else {
+                      String errorMessage = "";
+                      switch (response.error!.code) {
+                        case 100:
+                          {errorMessage = "ارتباط با سرور برقرار نشد!";}
+                          break;
+                        case 202:
+                          {errorMessage = "این نام کاربری قبلا استفاده شده";}
+                          break;
+                        case 203:
+                          {errorMessage = "این ایمیل قبلا استفاده شده";}
+                          break;
+                        case 200:
+                          {errorMessage = "نام کاربری انتخاب نکردی";}
+                          break;
+                        case 201:
+                          {errorMessage = "یادت رفت رمزعبور رو وارد کنی!";}
+                          break;
+                        case 204:
+                          {errorMessage = "یادت رفت ایمیلت رو وارد کنی!";}
+                          break;
+                        default:
+                          {response.toString();}
+                          break;
+                      }
+                      Get.defaultDialog(
+                          title: "خطا",
+                          titleStyle: TextStyle(color: Colors.red),
+                          content: Text(
+                            errorMessage,
+                          ),
+                          textConfirm: "باشه"
+                      );
+                    }
+                    return response;
+                  });;
                 },
                 child: Text("ثبت نام")),
             TextButton(
