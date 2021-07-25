dd4597c81ed9c47a035e794ccfa7a7dbca6319b9
Peyman-hme <peyman.eb13@gmail.com>
2021-07-25 16:39:18 +0430

    Home menu first implemented

--- lib/views/navigation_pages/home.dart
+++ lib/views/navigation_pages/home.dart
@@ -1,17 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'dart:ui' as ui;
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => Directionality(
-        textDirection: TextDirection.rtl,
-        child: Container(
-          child: ElevatedButton(
-            child: Text("کلیک بنمای"),
-            onPressed: () => Get.toNamed('/subjects'),
-          ),
+  Widget build(BuildContext context) => Center(
+        child: Column(
+          children: [
+            Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Image.asset(
+                  'assets/images/animated_lamp.gif',
+                  width: Get.width / 3,
+                  height: Get.width / 3,
+                )),
+            Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Text(
+                  "کوییزستان",
+                  style: TextStyle(fontFamily: 'Lalezar', fontSize: 40,foreground: Paint()
+                    ..shader = ui.Gradient.linear(
+                      const Offset(0, 350),
+                      const Offset(450, 350),
+                      <Color>[
+                        Colors.red,
+                        Colors.yellow,
+                      ],
+                    )),
+                )),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Container(
+                child: ElevatedButton(
+                  child: Text(
+                    "بازی جدید",
+                    style: TextStyle(fontFamily: 'Traffic'),
+                  ),
+                  onPressed: () => Get.toNamed('/subjects'),
+                ),
+              ),
+            )
+          ],
         ),
       );
 }
