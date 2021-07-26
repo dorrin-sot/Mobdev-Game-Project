07a108a4d6c513dfc646f703a30e86cdf7df8fe4
yasin459 <yasinnmoosavi@gmail.com>
2021-07-26 17:24:07 +0430

    snackbar added for guest trying to play game

--- lib/views/navigation_pages/home.dart
+++ lib/views/navigation_pages/home.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'dart:ui' as ui;
 
+import 'package:mobdev_game_project/main.dart';
+
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  HomePage({Key? key}) : super(key: key);
+  AppController _appController = Get.find<AppController>();
 
   @override
   Widget build(BuildContext context) => Center(
@@ -20,15 +23,18 @@ class HomePage extends StatelessWidget {
                 padding: EdgeInsets.all(16.0),
                 child: Text(
                   "کوییزستان",
-                  style: TextStyle(fontFamily: 'Lalezar', fontSize: 40,foreground: Paint()
-                    ..shader = ui.Gradient.linear(
-                      const Offset(0, 350),
-                      const Offset(450, 350),
-                      <Color>[
-                        Colors.red,
-                        Colors.yellow,
-                      ],
-                    )),
+                  style: TextStyle(
+                      fontFamily: 'Lalezar',
+                      fontSize: 40,
+                      foreground: Paint()
+                        ..shader = ui.Gradient.linear(
+                          const Offset(0, 350),
+                          const Offset(450, 350),
+                          <Color>[
+                            Colors.red,
+                            Colors.yellow,
+                          ],
+                        )),
                 )),
             Padding(
               padding: EdgeInsets.all(16.0),
@@ -38,11 +44,25 @@ class HomePage extends StatelessWidget {
                     "بازی جدید",
                     style: TextStyle(fontFamily: 'Traffic'),
                   ),
-                  onPressed: () => Get.toNamed('/subjects'),
+                  onPressed: onPressed,
                 ),
               ),
             )
           ],
         ),
       );
+
+  void onPressed() {
+    if (_appController.isLoggedIn.value) {
+      Get.toNamed('/subjects');
+      return;
+    } else {
+      Get.snackbar(
+        "امکان ورود برای شما محیا نیست",
+        "اول باید ثبت نام کنی",
+        icon: Icon(Icons.person, color: Colors.black),
+        snackPosition: SnackPosition.BOTTOM,
+      );
+    }
+  }
 }
